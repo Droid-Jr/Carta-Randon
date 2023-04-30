2cc6aca6d93bdb26b7ef6cd76291eda53dd807ce
mario <118136625+Droid-Jr@users.noreply.github.com>
2023-04-30 14:12:30 -0500

    mario

---
+++ src/components/Animes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Animes() {
+  return (
+    <div className=' flex justify-center '>
+      <img className='w-[500px]  ' src="/naruto.gif" />
+    </div>
+  )
+}
+
+export default Animes
