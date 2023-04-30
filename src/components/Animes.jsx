e49e7d9c65be8a06e3e4d4999c75a23f24dd1286
mario <118136625+Droid-Jr@users.noreply.github.com>
2023-04-30 14:22:54 -0500

    mario

--- src/components/Animes.jsx
+++ src/components/Animes.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Animes() {
   return (
     <div className=' flex justify-center '>
-      <img className='w-[500px]  ' src="/naruto.gif" />
+      <img className='w-[500px]  ' src="/naruto.gif" loading='lazy'/>
     </div>
   )
 }
